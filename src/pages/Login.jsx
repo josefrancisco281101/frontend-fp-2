5527911d56ac165acd4aef224441c6e2dde21ebb
josefrancisco <josefrancisco281101@gmail.com>
2024-10-08 13:52:28 -0400

    estilos login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import {  useContext } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
 function Login() {
@@ -15,33 +15,47 @@ function Login() {
   };
 
   return (
-    <main className='h-screen flex flex-col justify-center items-center max-w-[300px] m-auto gap-5 lg:max-w-[800px]'>
-      <h2 className='text-2xl'>Iniciar sesión</h2>
-      <form
-        onSubmit={handleLogin}
-        className='border-2 border-blue-400 rounded-md p-5 flex flex-col gap-3'
-      >
-        <label className='text-md'>
-          Nombre de usuario:
-          <input
-            type='text'
-            name='username'
-            className='border border-gray-400 rounded-md w-full p-2'
-          />
-        </label>
-        <label className='text-md'>
-          Contraseña:
-          <input
-            type='password' 
-            name='password'
-            className='border border-gray-400 rounded-md w-full p-2'
-          />
-        </label>
-        <button type='submit'>Ingresar</button>
-        {errorMessage && (
-          <p className='text-red-500 mt-4 text-center'>{errorMessage}</p>
-        )}
-      </form>
+    <main className='h-screen flex flex-col justify-center items-center bg-gray-50'>
+      <div className='max-w-[400px] w-full bg-white shadow-lg rounded-lg p-6 m-4'>
+        <h2 className='text-3xl font-semibold text-center text-blue-600 mb-6'>
+          Iniciar sesión
+        </h2>
+        <form
+          onSubmit={handleLogin}
+          className='flex flex-col gap-4'
+        >
+          <label className='text-md'>
+            Nombre de usuario:
+            <input
+              type='text'
+              name='username'
+              className='border border-gray-300 rounded-md w-full p-3 mt-1 focus:ring-2 focus:ring-blue-400 focus:outline-none transition duration-200'
+              placeholder='Introduce tu nombre de usuario'
+            />
+          </label>
+          <label className='text-md'>
+            Contraseña:
+            <input
+              type='password' 
+              name='password'
+              className='border border-gray-300 rounded-md w-full p-3 mt-1 focus:ring-2 focus:ring-blue-400 focus:outline-none transition duration-200'
+              placeholder='Introduce tu contraseña'
+            />
+          </label>
+          <button
+            type='submit'
+            className='w-full bg-blue-500 text-white py-3 rounded-md hover:bg-blue-600 transition duration-300'
+          >
+            Ingresar
+          </button>
+          {errorMessage && (
+            <p className='text-red-500 mt-4 text-center'>
+              {errorMessage}
+            </p>
+          )}
+        </form>
+       
+      </div>
     </main>
   );
 }
